d6c885df2d7a6b85a27e6b0017558bb2f9f798dc
Carlos Fernandez <carlosfa99@outlook.es>
2025-12-27 19:24:54 +0100

    feat: add post page

--- app/pages/studio/posts/[url].vue
+++ app/pages/studio/posts/[url].vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: "studio",
+import PostForm from "~/components/posts/post-form.vue";
+import { usePostFormStore } from "~/posts/post-form-store";
+import type { Post, PostActionType } from "~/posts/types";
+
+definePageMeta({ layout: "studio" });
+
+const toast = useToast();
+const route = useRoute();
+const isNew = route.params.url === "new";
+
+const post = ref<Partial<Post>>(
+  isNew ? { title: "" } : { title: "Sample Post Title", id: 1, published: true }
+);
+
+const postFormStore = usePostFormStore();
+postFormStore.init(post.value);
+
+postFormStore.onSuccess((updatedPost: Post, action: PostActionType) => {
+  let title = "Post saved successfully";
+  if (action === "publish") title = "Post published successfully";
+  else if (action === "unpublish") title = "Post unpublished successfully";
+
+  toast.add({ title });
+
+  if (action === "save") {
+    // redirect to new post URL (check if the updated post has different url before redirecting)
+  }
 });
 
-const route = useRoute();
-
-const url = route.params.url as string;
+postFormStore.onError((error: string, action: PostActionType) => {
+  toast.add({ title: `Error: ${error}` });
+});
 </script>
 
 <template>
-  <h1>Post URL: {{ url }}</h1>
+  <Upage>
+    <PostForm />
+  </Upage>
 </template>
