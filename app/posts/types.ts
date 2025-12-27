b0525cf45f3b2be24b0b1b6191e89e7bcd1ee808
Carlos Fernandez <carlosfa99@outlook.es>
2025-12-27 19:21:58 +0100

    feat: add post types

---
+++ app/posts/types.ts
@@ -0,0 +1,21 @@
+import type { ButtonProps } from "@nuxt/ui";
+
+export type Post = {
+  id: number;
+  title: string;
+  subtitle?: string;
+  content: string;
+  published: boolean;
+  publishedAt?: Date;
+  updatedAt: Date;
+  createdAt: Date;
+};
+
+export type PostActionType = "save" | "publish" | "unpublish";
+
+export type PostAction = {
+  type: PostActionType;
+  label: string;
+  icon: string;
+  color: ButtonProps["color"];
+};
