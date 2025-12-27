c6fcd2244d887dc03fa523bdcbee5948b1c96241
Guillermo Cifre <guillecg13@gmail.com>
2025-12-27 20:05:04 +0100

    chore: apply formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
