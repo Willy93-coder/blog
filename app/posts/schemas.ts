87f6cd281bc9316df7759db1a96c371449fff148
Carlos Fernandez <carlosfa99@outlook.es>
2025-12-27 19:22:09 +0100

    feat: add post form schema

---
+++ app/posts/schemas.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const postFormSchema = z.object({
+  title: z.string("Title is required").min(1, "Title is required"),
+});
+
+export type PostForm = z.infer<typeof postFormSchema>;
