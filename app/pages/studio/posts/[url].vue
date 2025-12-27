f18d9615e36eeb98b2abfd2079410b236781957c
Carlos Fernandez <carlosfa99@outlook.es>
2025-12-27 08:25:29 +0100

    feat: add create edit post page file

---
+++ app/pages/studio/posts/[url].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "studio",
+});
+
+const route = useRoute();
+
+const url = route.params.url as string;
+</script>
+
+<template>
+  <h1>Post URL: {{ url }}</h1>
+</template>
