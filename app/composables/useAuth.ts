2c01233d22fc13148b4bfb7d27114635e3143fb1
Guillermo Cifre <guillecg13@gmail.com>
2026-01-05 17:47:44 +0100

    feat: add useAuth composable

---
+++ app/composables/useAuth.ts
@@ -0,0 +1,24 @@
+const AuthService = () => {
+  const { $supabase } = useNuxtApp();
+  const appUrl = useAppUrl();
+
+  return {
+    signInWithGitHub: async () =>
+      await $supabase.auth.signInWithOAuth({
+        provider: "github",
+        options: {
+          redirectTo: appUrl.absoluteStudio({ toast: "login_success" }),
+        },
+      }),
+    signOut: async () => await $supabase.auth.signOut(),
+    isAllowedUser: async () => {
+      const { data, error } = await $supabase.rpc("is_allowlisted");
+
+      if (error) return { allowed: false, error };
+
+      return { allowed: Boolean(data), error: null };
+    },
+  };
+};
+
+export { AuthService };
