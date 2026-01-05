21da89abd2413ad76803168a5f1c410515c21786
Guillermo Cifre <guillecg13@gmail.com>
2026-01-05 17:40:56 +0100

    chore: add public site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,12 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   runtimeConfig: {
     public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL,
       supabaseUrl: process.env.SUPABASE_URL,
       supabasePublishKey: process.env.SUPABASE_PUBLISHABLE_KEY,
     },
+  },
+  routeRules: {
+    "/studio/**": { ssr: false },
   },
 });
