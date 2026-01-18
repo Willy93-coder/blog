7e2cd23cad5ff94d24fa75d166f4d9b667a977f7
Guillermo Cifre <guillecg13@gmail.com>
2026-01-18 20:06:27 +0100

    chore: add explicit imports in auth.global

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,9 +1,13 @@
+import { defineNuxtRouteMiddleware, navigateTo, useNuxtApp } from '#app';
+import { useRoutes } from '~/composables/useRoutes';
+import { useAuth } from '~/composables/useAuth';
+
 export default defineNuxtRouteMiddleware(async (to) => {
   // Public URLs
   if (to.meta.public) return;
 
   const routes = useRoutes();
-  const auth = AuthService();
+  const auth = useAuth();
   const { $supabase } = useNuxtApp();
 
   const { data, error } = await $supabase.auth.getSession();
