08678e706124cfc5da35634573e5b0cc5d8dd664
Guillermo Cifre <guillecg13@gmail.com>
2026-02-21 12:32:36 +0100

    feat: add dynamic public post page

--- app/pages/posts/[url].vue
+++ app/pages/posts/[url].vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+  import PostPublic from '~/components/posts/PostPublic.vue';
+  import type { PostWithTags } from '~/types/post';
+
   definePageMeta({
     public: true,
     layout: 'blog',
   });
+
+  const route = useRoute();
+  const postActions = usePosts();
+
+  const { data: post } = await useAsyncData<PostWithTags | null>(`post-${route.params.url}`, async () => {
+    const { data, error } = await postActions.getPostWithTagsById({ id: route.params.url as string });
+    if (error !== null || !data || !data.published) return null;
+    return data;
+  });
+
+  if (!post.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Post not found' });
+  }
 </script>
-<template></template>
+
+<template>
+  <PostPublic :post="post!" />
+</template>
