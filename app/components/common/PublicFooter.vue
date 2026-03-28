69f41d18e1e0154745d39ea64abfe8e74d97bc37
Guillermo Cifre <guillecg13@gmail.com>
2026-03-28 19:23:50 +0100

    fix: update footer copyright name to GMV-Blog

--- app/components/common/PublicFooter.vue
+++ app/components/common/PublicFooter.vue
@@ -5,7 +5,7 @@
 <template>
   <footer class="w-full border-t border-default bg-default dark:border-default dark:bg-default">
     <div class="mx-auto max-w-3xl px-4 py-6">
-      <p class="text-center text-sm text-dimmed">&copy; {{ year }} My Blog. All rights reserved.</p>
+      <p class="text-center text-sm text-dimmed">&copy; {{ year }} GMV-Blog. All rights reserved.</p>
     </div>
   </footer>
 </template>
